c094b332ad8dc0ec5e1c6d64bf8ea4e2233616f0
dewiHF <danielkang055@gmail.com>
2022-04-19 15:05:45 +0800

    form login dan folder assetlogin

---
+++ login.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags-->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Title Page-->
+    <title>Login</title>
+
+    <!-- Icons font CSS-->
+    <link href="assetlogin/vendor/mdi-font/css/material-design-iconic-font.min.css" rel="stylesheet" media="all">
+    <link href="assetlogin/vendor/font-awesome-4.7/css/font-awesome.min.css" rel="stylesheet" media="all">
+
+    <!-- Vendor CSS-->
+    <link href="assetlogin/vendor/select2/select2.min.css" rel="stylesheet" media="all">
+    <link href="assetlogin/vendor/datepicker/daterangepicker.css" rel="stylesheet" media="all">
+
+    <!-- Main CSS-->
+    <link href="assets/css/login.css" rel="stylesheet" media="all">
+</head>
+
+<body>
+    <div class="page-wrapper bg-gra-02 p-t-130 p-b-100 font-poppins">
+        <div class="wrapper wrapper--w680">
+            <div class="card card-4">
+                <div class="card-body">
+                    <h2 class="title">Login Form</h2>
+                    <form method="POST">
+                        <div class="row row-space">
+                            <div class="col-2">
+                                <div class="input-group">
+                                    <label class="label">Nama</label>
+                                    <input class="input--style-4" type="text" name="first_name">
+                                </div>
+                            </div>
+                            <div class="col-2">
+                                <div class="input-group">
+                                    <label class="label">Password</label>
+                                    <input class="input--style-4" type="password" name="last_name">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="input-group">
+                            <label class="label">Level</label>
+                            <div class="rs-select2 js-select-simple select--no-search">
+                                <select name="level">
+                                    <option disabled="disabled" selected="selected">Choose option</option>
+                                    <option>Admin</option>
+                                    <option>Instruktur/Asisten Instruktur</option>
+                                </select>
+                                <div class="select-dropdown"></div>
+                            </div>
+                        </div>
+                        <div class="p-t-15">
+                            <button class="btn btn--radius-2 btn--blue" type="submit">Submit</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Jquery JS-->
+    <script src="assetlogin/vendor/jquery/jquery.min.js"></script>
+    <!-- Vendor JS-->
+    <script src="assetlogin/vendor/select2/select2.min.js"></script>
+    <script src="assetlogin/vendor/datepicker/moment.min.js"></script>
+    <script src="assetlogin/vendor/datepicker/daterangepicker.js"></script>
+
+    <!-- Main JS-->
+    <script src="assetlogin/js/global.js"></script>
+
+</body><!-- This templates was made by Colorlib (https://colorlib.com) -->
+
+</html>
+<!-- end document-->
