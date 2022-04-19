d027537637fa993224b86da827feac6f71eb3547
MiftahulRahman <miftahulrahmanip03@gmail.com>
2022-04-19 15:30:26 +0800

    Visi Misi

---
+++ diklat.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<img class="auth-circle" src="/static/media/auth-bg.45e29c2f.svg" alt="background-circle">
+<div class="header-text"><h2>Pendaftaran</h2></div>
+<p class="paragraph text-16 mb-4">Mohon isi data berikut dengan benar.</p>
+<form><div class="form-group"><label class="form-label">Alamat E-mail</label><input id="text" class="form-control  " autocomplete="off" value=""></div><div class="form-group"><label class="form-label">Password</label><img src="/static/media/icon-eyes.4157b09e.svg" class="form-icon" alt="icon"><input id="password" class="form-control  " type="password" value=""></div><div class="form-group"><label class="form-label">Ulangi Password</label><img src="/static/media/icon-eyes.4157b09e.svg" class="form-icon" alt="icon"><input id="confirmPassword" class="form-control  " type="password" value=""></div><div class="mb-4"><button type="submit" class="btn btn-primary btn-block">Daftar</button></div><div class="d-flex mt-4"><div class="form-check"><input type="checkbox" class="form-check-input position-static"></div><div class="text-12 ml-1"><span class="mr-1">Saya menyetujui</span><span class="mr-1"><a target="_blank" rel="noreferrer" href="https://www.prakerja.go.id/syarat-ketentuan">Syarat dan Ketentuan</a></span><span class="mr-1">dan</span><span class="mr-1"><a target="_blank" rel="noreferrer" href="https://www.prakerja.go.id/kebijakan-privasi">Kebijakan Privasi</a></span><span>yang berlaku</span></div></div></form>
+<hr>
+<div class="mt-4"><span class="mr-1">Sudah  akun?</span><span class="mr-1"><a href="/masuk">Masuk di sini</a></span></div>
+</body>
+</html>
